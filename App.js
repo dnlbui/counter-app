e5aff24b5db0ffa662b7bc4ce45cf819eb34adcc
Daniel Bui <dnlbui@proton.me>
2023-02-24 15:37:11 -0600

    imported button, useState for counter

--- App.js
+++ App.js
@@ -1,15 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// be carefule installing react native paper bc using expo... change button and align them...
+
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [ counter, setCounter ] = useState(0);
+
   return (
+    // similar to div
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{counter}</Text>
+      <Button
+        onPress={()=>setCounter(counter+1)}
+        title='Increment'
+      />
+      <Button
+        onPress={()=>setCounter(counter-1)}
+        title='Decrement'
+      />
     </View>
   );
 }
-
+// react native default flex
 const styles = StyleSheet.create({
   container: {
     flex: 1,
