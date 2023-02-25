4153dec4975e0d7eba6de533406a0c490f696286
Daniel Bui <dnlbui@proton.me>
2023-02-24 20:13:21 -0600

    import safearea+statusbar. add button attributes

--- App.js
+++ App.js
@@ -1,28 +1,50 @@
 // be carefule installing react native paper bc using expo... change button and align them...
 
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, SafeAreaView } from 'react-native';
 import { useState } from 'react';
-import { Provider as PaperProvider, Button, List, useTheme} from 'react-native-paper';
+import { Button } from 'react-native-paper';
 
 export default function App() {
   const [ counter, setCounter ] = useState(0);
 
   return (
-    <PaperProvider>
-      <View style={styles.container}>
-        <Text>{counter}</Text>
-        <Button
-          onPress={()=>setCounter(counter+1)}
-          title='Increment'
-        />
-        <Button
-          onPress={()=>setCounter(counter-1)}
-          title='Decrement'
-        />
-      </View>
-    </PaperProvider>
+    <>
+      
+      <SafeAreaView style={styles.container}>
+        <StatusBar barStyle="dark-content" />
+        <View style={styles.row}>
+          <Text style={styles.titleText}>Counter App</Text>
+        </View>
+
+        <View style={styles.row}>
+          <Text>{counter}</Text>
+        </View>
+        
+        <View style={styles.parent}>
+          <Button
+            theme={{ roundness: 3 }}
+            mode="outlined"
+            icon="minus"
+            compact="true"
+            onPress={()=>setCounter(counter-1)}
+            title='Decrement'
+          >Decrement</Button>
+          <Button
+            //theme={{ roundness: 3 }}
+            style={styles}
+            mode="outlined"
+            icon="plus"
+            compact="true"
+            onPress={()=>setCounter(counter+1)}
+            title='Increment'
+          >Increment</Button>
+        </View>
+
+      </SafeAreaView>
+    </>
   );
 }
+
 // react native default flex
 const styles = StyleSheet.create({
   container: {
@@ -30,5 +52,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  parent: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
