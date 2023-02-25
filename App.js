2dbac931145dfb45813fe1962d50cf9397cd0f71
Daniel Bui <dnlbui@proton.me>
2023-02-24 18:03:26 -0600

    wrap paper provier and import button,list,useTheme

--- App.js
+++ App.js
@@ -2,23 +2,25 @@
 
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { useState } from 'react';
+import { Provider as PaperProvider, Button, List, useTheme} from 'react-native-paper';
 
 export default function App() {
   const [ counter, setCounter ] = useState(0);
 
   return (
-    // similar to div
-    <View style={styles.container}>
-      <Text>{counter}</Text>
-      <Button
-        onPress={()=>setCounter(counter+1)}
-        title='Increment'
-      />
-      <Button
-        onPress={()=>setCounter(counter-1)}
-        title='Decrement'
-      />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text>{counter}</Text>
+        <Button
+          onPress={()=>setCounter(counter+1)}
+          title='Increment'
+        />
+        <Button
+          onPress={()=>setCounter(counter-1)}
+          title='Decrement'
+        />
+      </View>
+    </PaperProvider>
   );
 }
 // react native default flex
